62d84a25ccfe33e93d50444a3814421d89a9bc90
Jandy <hades.ch@gmail.com>
2020-06-27 17:02:37 +0800

    Update utils.py

--- imbox/utils.py
+++ imbox/utils.py
@@ -11,6 +11,8 @@ def str_decode(value='', encoding=None, errors='strict'):
     if isinstance(value, str):
         return bytes(value, encoding, errors).decode('utf-8')
     elif isinstance(value, bytes):
+        # 解决 windows-874 解码, cp874 = windows-874 别名
+        _encoding = 'cp874' if encoding == 'windows-874' else encoding
         return value.decode(encoding or 'utf-8', errors=errors)
     else:
         raise TypeError("Cannot decode '{}' object".format(value.__class__))
