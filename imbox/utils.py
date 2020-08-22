728dcfbf70637a997e08125bc5ae165cfe398001
Jandy <hades.ch@gmail.com>
2020-08-23 05:41:10 +0800

    解决越南语 viscii 编码

--- imbox/utils.py
+++ imbox/utils.py
@@ -13,6 +13,8 @@ def str_decode(value='', encoding=None, errors='strict'):
     elif isinstance(value, bytes):
         # 解决 windows-874 解码, cp874 = windows-874 别名
         _encoding = 'cp874' if encoding == 'windows-874' else encoding
-        return value.decode(encoding or 'utf-8', errors=errors)
+        # return value.decode(encoding or 'utf-8', errors=errors)
+        # 解决 viscii 编码异常
+        return value.decode(_encoding or 'utf-8', errors='ignore' if encoding=='viscii' else errors)
     else:
         raise TypeError("Cannot decode '{}' object".format(value.__class__))
